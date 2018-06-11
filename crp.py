a89c1899b266156145160da293ca89178fc5dd19
kieranjol <kieran.o.leary@gmail.com>
2018-06-11 20:00:01 +0100

    rearranges functions

--- crp.py
+++ crp.py
@@ -9,103 +9,6 @@ import json
 import subprocess
 import lxml.etree as ET
 
-def get_exiftool_json(source):
-    '''
-    Returns JSON object of exiftool output
-    '''
-    exiftool_json = subprocess.check_output([
-        'exiftool',
-        '-J',
-        source
-    ])
-    parsed = json.loads(exiftool_json)
-    print json.dumps(parsed, indent=4, sort_keys=True)
-    return parsed[0]
-
-
-def make_dc_object():
-    '''
-    Generates a minimal lxml Dublin Core object containing the DC namespace header.
-    '''
-    header = "<metadata xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:dc='http://purl.org/dc/elements/1.1/' xmlns:dcterms='http://purl.org/dc/terms/'></metadata>"
-    dublin_core_object = ET.ElementTree(ET.fromstring(header))
-    return dublin_core_object
-
-def add_dc_elements(root_metadata_element, dc_namespace):
-    '''
-    Adds some of the basic DC elements to the XML object.
-    '''
-    counter = 1
-    element_list = []
-    for elements in [
-            'identifier',
-            'provenance',
-            'provenance',
-            'type',
-            'format',
-            'title',
-            'creator',
-            'rights',
-            'rights',
-            'language',
-            'date'
-        ]:
-        element = create_dc_element(
-            index=counter,
-            parent=root_metadata_element,
-            dc_element=elements,
-            dublin_core_namespace=dc_namespace
-        )
-        element_list.append(element)
-        counter += 1
-    return element_list
-def csv_extract(csv_file):
-    '''
-    Read the csv and store the data in a list of dictionaries.
-    '''
-    object_dictionaries = []
-    input_file = csv.DictReader(open(csv_file))
-    for rows in input_file:
-        object_dictionaries.append(rows)
-    return object_dictionaries
-
-
-def create_dc_element(index, parent, dc_element, dublin_core_namespace):
-    '''
-    Adds an empty metadata element to dublin_core_object
-    Args:
-    index = Order in which element should appear below parent.
-    parent = Parent element of the new element.
-    dc_element = Name, without namespace of your Dublin Core element.
-    dublin_core_namespace = Literally the DC namespace.
-    Returns: The element object itself, just incase the script needs to access it.
-    '''
-    dc_element = ET.Element("{%s}%s" % (dublin_core_namespace, dc_element))
-    parent.insert(index, dc_element)
-    return dc_element
-
-def extract_checksum(manifest):
-    '''
-    Extracts the MD5 checksum from a manifest.
-    '''
-    with open(manifest, 'r') as manifest_object:
-        manifest_line = manifest_object.readlines()
-    checksum = manifest_line[0][:32]
-    return checksum
-
-def create_assets_element(index, parent, dc_element):
-    '''
-    Adds an empty metadata element to dublin_core_object
-    Args:
-    index = Order in which element should appear below parent.
-    parent = Parent element of the new element.
-    dc_element = Name, without namespace of your Dublin Core element.
-    dublin_core_namespace = Literally the DC namespace.
-    Returns: The element object itself, just incase the script needs to access it.
-    '''
-    dc_element = ET.Element(dc_element)
-    parent.insert(index, dc_element)
-    return dc_element
 
 def parse_args():
     '''
@@ -125,6 +28,95 @@ def parse_args():
     )
     parsed_args = parser.parse_args()
     return parsed_args
+
+
+def csv_extract(csv_file):
+    '''
+    Read the csv and store the data in a list of dictionaries.
+    '''
+    object_dictionaries = []
+    input_file = csv.DictReader(open(csv_file))
+    for rows in input_file:
+        object_dictionaries.append(rows)
+    return object_dictionaries
+
+
+def analyse_folder(folder_name):
+    '''
+    Analyze a folder to figure out how complex he package is. Create dictionary
+    that lists checksum path, access copy, preservation copy.
+    '''
+    file_info_list = []
+    contents = sorted(os.listdir(folder_name))
+    for files in contents:
+        if files.endswith('prsv.tif'):
+            dictionary = {}
+            dictionary[
+                'Preservation'
+            ] = os.path.join(folder_name, files)
+            dictionary[
+                'Access'
+            ] = os.path.join(folder_name, files.replace('prsv.tif', 'access.jpg'))
+            dictionary[
+                'access_checksum'
+            ] = dictionary['Access'] + '.md5'
+            dictionary[
+                'master_checksum'
+            ] = dictionary['Preservation'] + '.md5'
+            file_info_list.append(dictionary)
+    return file_info_list
+
+
+def add_DC_metadata(folder, dc_namespace, xsi_namespace, csv_record):
+    print('Found %s, processing...') % folder
+    dublin_core_object = make_dc_object()
+    # Sets up a bunch of empty Dublin Core XML elements.
+    root_metadata_element = dublin_core_object.getroot()
+    (
+        dc_identifier,
+        dc_crp_provenance,
+        dc_provenance,
+        dc_type,
+        dc_format,
+        dc_title,
+        dc_creator,
+        dc_rights,
+        dc_rights_country,
+        dc_language,
+        dc_date
+    ) = add_dc_elements(root_metadata_element, dc_namespace)
+    # Populate the empty elements with the corresponding CSV field.
+    dc_identifier.attrib["{%s}type" % xsi_namespace] = "dcterms:URI"
+    dc_rights_country.attrib["type"] = 'Country of Creation'
+    dc_rights.text = csv_record['Copyright Statement']
+    dc_rights_country.text = csv_record['Country of Creation']
+    dc_crp_provenance.text = 'California Revealed Project'
+    dc_provenance.text = csv_record['Institution']
+    dc_format.text = csv_record['Generation']
+    dc_title.text = csv_record['Main or Supplied Title']
+    dc_creator.text = csv_record['Creator']
+    dc_identifier.text = csv_record['Internet Archive URL']
+    dc_type.text = csv_record['Type']
+    dc_date.attrib["type"] = 'Issued'
+    dc_date.text = csv_record['Date Published']
+    dc_language.text = csv_record['Language']
+    return root_metadata_element, dublin_core_object
+
+
+def create_dc_element(index, parent, dc_element, dublin_core_namespace):
+    '''
+    Adds an empty metadata element to dublin_core_object
+    Args:
+    index = Order in which element should appear below parent.
+    parent = Parent element of the new element.
+    dc_element = Name, without namespace of your Dublin Core element.
+    dublin_core_namespace = Literally the DC namespace.
+    Returns: The element object itself, just incase the script needs to access it.
+    '''
+    dc_element = ET.Element("{%s}%s" % (dublin_core_namespace, dc_element))
+    parent.insert(index, dc_element)
+    return dc_element
+
 
 def add_asset_elements(root_metadata_element):
     '''
@@ -156,6 +148,86 @@ def add_asset_elements(root_metadata_element):
         dc_element='AssetPart'
         )
     return asset_element_list, AssetPart_element
+
+
+def get_exiftool_json(source):
+    '''
+    Returns JSON object of exiftool output
+    '''
+    exiftool_json = subprocess.check_output([
+        'exiftool',
+        '-J',
+        source
+    ])
+    parsed = json.loads(exiftool_json)
+    # print json.dumps(parsed, indent=4, sort_keys=True)
+    return parsed[0]
+
+
+def make_dc_object():
+    '''
+    Generates a minimal lxml Dublin Core object containing the DC namespace header.
+    '''
+    header = "<metadata xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:dc='http://purl.org/dc/elements/1.1/' xmlns:dcterms='http://purl.org/dc/terms/'></metadata>"
+    dublin_core_object = ET.ElementTree(ET.fromstring(header))
+    return dublin_core_object
+
+
+def add_dc_elements(root_metadata_element, dc_namespace):
+    '''
+    Adds some of the basic DC elements to the XML object.
+    '''
+    counter = 1
+    element_list = []
+    for elements in [
+            'identifier',
+            'provenance',
+            'provenance',
+            'type',
+            'format',
+            'title',
+            'creator',
+            'rights',
+            'rights',
+            'language',
+            'date'
+        ]:
+        element = create_dc_element(
+            index=counter,
+            parent=root_metadata_element,
+            dc_element=elements,
+            dublin_core_namespace=dc_namespace
+        )
+        element_list.append(element)
+        counter += 1
+    return element_list
+
+
+def extract_checksum(manifest):
+    '''
+    Extracts the MD5 checksum from a manifest.
+    '''
+    with open(manifest, 'r') as manifest_object:
+        manifest_line = manifest_object.readlines()
+    checksum = manifest_line[0][:32]
+    return checksum
+
+
+def create_assets_element(index, parent, dc_element):
+    '''
+    Adds an empty metadata element to dublin_core_object
+    Args:
+    index = Order in which element should appear below parent.
+    parent = Parent element of the new element.
+    dc_element = Name, without namespace of your Dublin Core element.
+    dublin_core_namespace = Literally the DC namespace.
+    Returns: The element object itself, just incase the script needs to access it.
+    '''
+    dc_element = ET.Element(dc_element)
+    parent.insert(index, dc_element)
+    return dc_element
+
+
 def create_instantiations(AssetPart_element, instantation_counter, generation):
     '''
     Create instantiations and build relationships.
@@ -208,65 +280,6 @@ def create_instantiations(AssetPart_element, instantation_counter, generation):
         counter += 1
     return instantiation_element_list
 
-def analyse_folder(folder_name):
-    '''
-    Analyze a folder to figure out how complex he package is. Create dictionary
-    that lists checksum path, access copy, preservation copy.
-    '''
-    file_info_list = []
-    contents = sorted(os.listdir(folder_name))
-    for files in contents:
-        if files.endswith('prsv.tif'):
-            dictionary = {}
-            dictionary[
-                'Preservation'
-            ] = os.path.join(folder_name, files)
-            dictionary[
-                'Access'
-            ] = os.path.join(folder_name, files.replace('prsv.tif', 'access.jpg'))
-            dictionary[
-                'access_checksum'
-            ] = dictionary['Access'] + '.md5'
-            dictionary[
-                'master_checksum'
-            ] = dictionary['Preservation'] + '.md5'
-            file_info_list.append(dictionary)
-    return file_info_list
-
-def add_DC_metadata(folder, dc_namespace, xsi_namespace, csv_record):
-    print('Found %s, processing...') % folder
-    dublin_core_object = make_dc_object()
-    # Sets up a bunch of empty Dublin Core XML elements.
-    root_metadata_element = dublin_core_object.getroot()
-    (
-        dc_identifier,
-        dc_crp_provenance,
-        dc_provenance,
-        dc_type,
-        dc_format,
-        dc_title,
-        dc_creator,
-        dc_rights,
-        dc_rights_country,
-        dc_language,
-        dc_date
-    ) = add_dc_elements(root_metadata_element, dc_namespace)
-    # Populate the empty elements with the corresponding CSV field.
-    dc_identifier.attrib["{%s}type" % xsi_namespace] = "dcterms:URI"
-    dc_rights_country.attrib["type"] = 'Country of Creation'
-    dc_rights.text = csv_record['Copyright Statement']
-    dc_rights_country.text = csv_record['Country of Creation']
-    dc_crp_provenance.text = 'California Revealed Project'
-    dc_provenance.text = csv_record['Institution']
-    dc_format.text = csv_record['Generation']
-    dc_title.text = csv_record['Main or Supplied Title']
-    dc_creator.text = csv_record['Creator']
-    dc_identifier.text = csv_record['Internet Archive URL']
-    dc_type.text = csv_record['Type']
-    dc_date.attrib["type"] = 'Issued'
-    dc_date.text = csv_record['Date Published']
-    dc_language.text = csv_record['Language']
-    return root_metadata_element, dublin_core_object
 
 def techncial_metadata(package_info, AssetPart_element, csv_record):
     '''
@@ -308,9 +321,10 @@ def techncial_metadata(package_info, AssetPart_element, csv_record):
                 fileExtension.text = exiftool_json["FileTypeExtension"]
                 # Strings needed as INTs returned for some reason..
                 if len(str(exiftool_json['BitsPerSample'])) > 1:
+                    # Probably best to find some other way of getting
+                    # a bits per pixel value rather than this
                     bits = str(exiftool_json['BitsPerSample']).split()
                     bits = [int(i) for i in bits]
-                    print bits
                     bitDepth.text = str(sum(bits))
                 else:
                     bitDepth.text = str(int(exiftool_json['BitsPerSample']) * int(exiftool_json["ColorComponents"]))
@@ -345,6 +359,8 @@ def techncial_metadata(package_info, AssetPart_element, csv_record):
                 except KeyError:
                     digitizerModel.getparent().remove(digitizerModel)
         instantiation_counter += 1
+
+
 def main():
     # Create args object which holds the command line arguments.
     args = parse_args()
@@ -403,6 +419,7 @@ def main():
                     with open(csv_record['Object Identifier'] + 'dc_metadata.xml', 'w') as outFile:
                         dublin_core_object.write(outFile, xml_declaration=True, encoding='UTF-8', pretty_print=True)
     print 'Transformed XML files have been saved in %s' % os.getcwd()
+
 
 if __name__ == '__main__':
     main()
